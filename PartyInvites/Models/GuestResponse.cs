4407c4f082862c75de1d7342454e34d1cdbb0e2c
Compunet IT <Compunet IT@DESKTOP-CKDGJ8C>
2019-09-20 16:33:46 +0100

    Add project files.

---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please Enter you Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Your Email")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please Enter a Valid Email Address" )]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please specify whether you'll attend")]
+        public bool? WillAttend { get; set; }
+    }
+}
